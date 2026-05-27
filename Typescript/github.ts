e888f18404c73af6aca77ace38fc75f649a38271
nchevula <naveenchevula221@gmail.com>
2026-05-27 14:51:23 +0530

    Update github.ts

---
+++ Typescript/github.ts
@@ -0,0 +1,19 @@
+// github/gitlab/bitbucket- version control tool 
+// To work with github - we need to have git installed on our system
+// we can download git from https://git-scm.com/downloads
+
+// 1. you are the first person to start test automation - No REPO AVAILABLE
+// 0. Create a new respository on the version control tool
+// 1. create a project and write test case in your local system
+// 2. initialize git in your local system - git init
+// 3. add the files to the staging area - git add .
+// local git area - working directory, staging area, local repository
+// 4. commit the changes - git commit -m "Initial commit"  - commit message should be meaningful and should describe the changes made in the commit 
+// 5. create branch - git branch <branch-name> - branches are used to work on different features or bug fixes without affecting the main codebase
+// git branch -M main - to rename the default branch to main // git branch -M master - to rename the default branch to master // git branch <old-branch-name> <new-branch-name> - to rename a branch
+// 6. git branch - List all the branches in the repository
+// 7. git remote add orgin https://github.com/naveenchevula221-lgtm/TestPW.git 
+// add a remote repo to your local repo linked with remote repository on github
+// 8. git push - u origin <branch-name> - to push the changes to the remote repository on github
+// 
+    
